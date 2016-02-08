0e4411d847bfc91d1fdfce9c479a48c6160b4d32
Isaiah Wesley <G00236117@W213-06.academic.raritanval.edu>
2016-02-08 12:26:45 -0500

    Made some changes to my friendly abode.

--- Picture.java
+++ Picture.java
@@ -1,34 +1,28 @@
+
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
- * This class was written as an early example for teaching Java with BlueJ.
+ * Write a description of class Picture here.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2011.07.31
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class Picture
 {
+    // instance variables - replace the example below with your own
     private Square wall;
     private Square window;
-    private Triangle roof;
-    private Circle sun;
+    private Triangle design;
+    private Circle ball;
 
     /**
      * Constructor for objects of class Picture
      */
     public Picture()
     {
-        // nothing to do... instance variables are automatically set to null
+        // initialise instance variables
     }
 
-    /**
-     * Draw this picture.
-     */
-    public void draw()
-    {
+public void draw()
+{
         wall = new Square();
         wall.moveHorizontal(-140);
         wall.moveVertical(20);
@@ -41,46 +35,41 @@ public class Picture
         window.moveVertical(40);
         window.changeSize(40);
         window.makeVisible();
+        
+        design = new Triangle();  
+        design.changeSize(60, 180);
+        design.moveHorizontal(20);
+        design.moveVertical(-60);
+        design.makeVisible();
 
-        roof = new Triangle();  
-        roof.changeSize(60, 180);
-        roof.moveHorizontal(20);
-        roof.moveVertical(-60);
-        roof.makeVisible();
-
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(100);
-        sun.moveVertical(-40);
-        sun.changeSize(80);
-        sun.makeVisible();
-    }
-
-    /**
-     * Change this picture to black/white display
-     */
+        ball = new Circle();
+        ball.changeColor("yellow");
+        ball.moveHorizontal(100);
+        ball.moveVertical(-40);
+        ball.changeSize(80);
+        ball.makeVisible();
+}
     public void setBlackAndWhite()
     {
         if (wall != null)   // only if it's painted already...
         {
             wall.changeColor("black");
             window.changeColor("white");
-            roof.changeColor("black");
-            sun.changeColor("black");
+           design.changeColor("black");
+            ball.changeColor("black");
         }
     }
-
-    /**
-     * Change this picture to use color display
-     */
     public void setColor()
     {
         if (wall != null)   // only if it's painted already...
         {
             wall.changeColor("red");
             window.changeColor("black");
-            roof.changeColor("green");
-            sun.changeColor("yellow");
+            design.changeColor("green");
+            ball.changeColor("yellow");
         }
     }
 }
+
+       
+
